1395dcc6167f880894c5f0c059d6d560f4645393
Konstantin Burov <burov.konstantin@gmail.com>
2012-04-22 12:02:11 +0300

    added currency interface

---
+++ Finance/ICurrency.cs
@@ -0,0 +1,13 @@
+﻿namespace Wanderer.Library.Finance
+{
+    /// <summary>
+    /// Currency interface.
+    /// </summary>
+    public interface ICurrency
+    {
+        /// <summary>
+        /// Currency name.
+        /// </summary>
+        string Name { get; }
+    }
+}
