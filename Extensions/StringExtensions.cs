b516bda220607d22002202de1f68bf408cdee8ac
Konstantin Burov <burov.konstantin@gmail.com>
2012-07-14 19:22:58 +0300

    Add StringExtensions class

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Wanderer.Library.Extensions
+{
+    /// <summary>
+    /// <see cref="string"/> extension methods.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Returns true if <paramref name="s"/> contains <see cref="System.Int32"/>
+        /// </summary>
+        public static bool IsInt32(this string s)
+        {
+            System.Int32 dummy;
+
+            return !string.IsNullOrEmpty(s) && System.Int32.TryParse(s, out dummy);
+        }
+
+        /// <summary>
+        /// Returns true if <paramref name="s"/> contains <see cref="System.UInt32"/>
+        /// </summary>
+        public static bool IsUInt32(this string s)
+        {
+            System.UInt32 dummy;
+
+            return !string.IsNullOrEmpty(s) && System.UInt32.TryParse(s, out dummy);
+        }
+    }
+}
