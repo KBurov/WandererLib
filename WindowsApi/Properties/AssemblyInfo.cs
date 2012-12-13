c820b6afd5fbffc0d63cf1fb6d508254270680f9
Konstantin Burov <konstantin.burov@hotmail.com>
2012-12-13 23:08:19 +0200

    Fix AssemblyInfo.cs

--- WindowsApi/Properties/AssemblyInfo.cs
+++ WindowsApi/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsApi")]
+[assembly: AssemblyTitle("Wanderer.Library.WindowsApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsApi")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("Wanderer")]
+[assembly: AssemblyProduct("Wanderer.Library.WindowsApi")]
+[assembly: AssemblyCopyright("Copyright © Wanderer 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
