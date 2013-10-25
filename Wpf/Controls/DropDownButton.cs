7310cfeaed9dccfd24c466865a8770ecee4c0b11
Konstantin Burov <konstantin.burov@hotmail.com>
2013-10-25 16:08:33 +0300

    Fix xml comments

--- Wpf/Controls/DropDownButton.cs
+++ Wpf/Controls/DropDownButton.cs
@@ -4,23 +4,44 @@ using System.Windows.Controls.Primitives;
 
 namespace Wanderer.Library.Wpf.Controls
 {
+    /// <summary>
+    /// Extended <see cref="ToggleButton"/> with drop-down menu.
+    /// </summary>
     public class DropDownButton : ToggleButton
     {
+        /// <summary>
+        /// Describes the position of drop-down menu.
+        /// </summary>
         public enum Placement
         {
+            /// <summary>
+            /// Drop-down menu appears at the bottom of button.
+            /// </summary>
             Bottom,
+            /// <summary>
+            /// Drop-down menu appears from the right side of the button.
+            /// </summary>
             Right
         }
 
+        /// <summary>
+        /// Describes the position of drop-down menu.
+        /// </summary>
         public Placement DropDownPlacement { private get; set; }
 
         #region DropDown (DependencyProperty)
+        /// <summary>
+        /// The <see cref="ContextMenu"/> object for drop-down menu.
+        /// </summary>
         public ContextMenu DropDown
         {
             get { return (ContextMenu) GetValue(DropDownProperty); }
             set { SetValue(DropDownProperty, value); }
         }
 
+        /// <summary>
+        /// Attached property for <see cref="DropDown"/>.
+        /// </summary>
         public static readonly DependencyProperty DropDownProperty =
             DependencyProperty.Register("DropDown", typeof (ContextMenu), typeof (DropDownButton),
                                         new PropertyMetadata(null, OnDropDownChanged));
