eec682e75a6be904fdb6fe6ec7c934ad9d9a1548
Konstantin Burov <konstantin.burov@hotmail.com>
2013-10-28 23:11:05 +0200

    Add EnvironmentEx class
    Provides additional information and methods to Environment class.

---
+++ Common/EnvironmentEx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Management;
+using System.Text;
+
+namespace Wanderer.Library.Common
+{
+    /// <summary>
+    /// Provides additional information and methods to <see cref="Environment" /> class.
+    /// </summary>
+    public static class EnvironmentEx
+    {
+        private const string InternalServicePackNumberVariableName = "ServicePackNumber";
+        private const string DefaultServicePackNumber = "0";
+
+        /// <summary>
+        /// Variable name for operating system service pack version number.
+        /// </summary>
+        public const string ServicePackNumberVariableName = "%" + InternalServicePackNumberVariableName + "%";
+
+        /// <summary>
+        /// Replaces the name of each environment variable embedded in the specified string with the string equivalent of the value of the variable,
+        /// then returns the resulting string.
+        /// </summary>
+        /// <param name="name">
+        /// a string containing the names of zero or more environment variables;
+        /// each environment variable is quoted with the percent sign character (%)
+        /// </param>
+        /// <returns>a string with each environment variable replaced by its value</returns>
+        public static string ExpandEnvironmentVariables(string name)
+        {
+            var nameParts = name.Split(new[] {'%'});
+            var builder = new StringBuilder(nameParts[0]);
+
+            for (var i = 1;i < nameParts.Length - 1;++i) {
+                if ((i % 2 == 0) || string.IsNullOrWhiteSpace(nameParts[i])) {
+                    continue;
+                }
+
+                builder.Append(InternalExpandEnvironmentVariable(nameParts[i]));
+            }
+
+            if (nameParts.Length > 2) {
+                builder.Append(nameParts[nameParts.Length - 1]);
+            }
+
+            return Environment.ExpandEnvironmentVariables(builder.ToString());
+        }
+
+        private static string InternalExpandEnvironmentVariable(string name)
+        {
+            if (name.Equals(InternalServicePackNumberVariableName, StringComparison.InvariantCultureIgnoreCase)) {
+                var query = new SelectQuery("Win32_OperatingSystem");
+
+                using (var searcher = new ManagementObjectSearcher(query)) {
+                    foreach (ManagementObject mo in searcher.Get()) {
+                        return mo["ServicePackMajorVersion"].ToString();
+                    }
+                }
+
+                return DefaultServicePackNumber;
+            }
+
+            return "%" + name + "%";
+        }
+    }
+}
