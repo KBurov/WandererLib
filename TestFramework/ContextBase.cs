6ae17d99456dc169e7bb5508a536019943417ee2
Konstantin Burov <konstantin.burov@hotmail.com>
2013-10-30 14:19:43 +0200

    Add ContextBase class

---
+++ TestFramework/ContextBase.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace Wanderer.Library.TestFramework
+{
+    /// <summary>
+    /// Base class for unit tests context classes.
+    /// </summary>
+    [TestFixture]
+    public abstract class ContextBase
+    {
+        /// <summary>
+        /// Setting up the test fixture (arrange) and run test action (<see cref="Arrange"/> and <see cref="Act"/>).
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            Arrange();
+
+            Act();
+        }
+
+        /// <summary>
+        /// Tear down the test fixture to return to the original state.
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            Cleanup();
+        }
+
+        /// <summary>
+        /// Setting up the test fixture.
+        /// </summary>
+        public virtual void Arrange() {}
+
+        /// <summary>
+        /// Execute test action.
+        /// </summary>
+        public virtual void Act() {}
+
+        /// <summary>
+        /// Tear down the test fixture to return to the original state.
+        /// </summary>
+        public virtual void Cleanup() {}
+    }
+}
