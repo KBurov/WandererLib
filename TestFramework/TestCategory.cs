f112794a6dbcd2f2feafe9353609cb4d158c0ba9
Konstantin Burov <konstantin.burov@hotmail.com>
2013-10-30 14:38:05 +0200

    Add TestCategory class
    Contains all available test categories.

---
+++ TestFramework/TestCategory.cs
@@ -0,0 +1,17 @@
+﻿namespace Wanderer.Library.TestFramework
+{
+    /// <summary>
+    /// Contains all available tests categories. 
+    /// </summary>
+    public static class TestCategory
+    {
+        /// <summary>
+        /// Unit test category.
+        /// </summary>
+        public const string Unit = "Unit";
+        /// <summary>
+        /// Integration test category.
+        /// </summary>
+        public const string Integration = "Integration";
+    }
+}
