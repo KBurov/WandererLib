e4f2b2aad308774d5c4686d3dde60adfbbed9218
Konstantin Burov <konstantin.burov@hotmail.com>
2013-12-24 15:44:45 +0200

    Re-write unit tests for DisposeAction

---
+++ Tests/Common.UnitTests/given_DisposeAction/with_not_null_action/when_call_dispose.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using Wanderer.Library.TestFramework;
+
+namespace Wanderer.Library.Common.UnitTests.given_DisposeAction.with_not_null_action
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable"), TestFixture]
+    [Category(TestCategory.Unit)]
+// ReSharper disable InconsistentNaming
+    public class when_call_dispose : ContextBase
+    {
+        private IDisposable _disposeAction;
+        private bool _actionCalled;
+
+        protected override void Act()
+        {
+            _disposeAction = new DisposeAction(() => { _actionCalled = true; });
+            _actionCalled = false;
+        }
+
+        [Test]
+        public void then_action_called()
+        {
+            using (_disposeAction) {}
+
+            Assert.IsTrue(_actionCalled);
+        }
+    }
+}
---
+++ Tests/Common.UnitTests/given_DisposeAction/with_null_action/when_call_constructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using Wanderer.Library.TestFramework;
+
+namespace Wanderer.Library.Common.UnitTests.given_DisposeAction.with_null_action
+{
+    [TestFixture]
+    [Category(TestCategory.Unit)]
+// ReSharper disable InconsistentNaming
+    public class when_call_constructor
+    {
+        [Test]
+        [ExpectedException(typeof (ArgumentNullException))]
+        public void then_exception_occurs()
+        {
+            using (new DisposeAction(null)) {}
+        }
+    }
+}
