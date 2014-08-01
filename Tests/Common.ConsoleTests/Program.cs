1f9d462328909ad946c89d8e7af4f13e97157a51
Konstantin Burov <konstantin.burov@hotmail.com>
2014-08-01 08:18:32 +0300

    Cosmetic change

--- Tests/Common.ConsoleTests/Program.cs
+++ Tests/Common.ConsoleTests/Program.cs
@@ -20,28 +20,30 @@ namespace Wanderer.Library.Common.ConsoleTests
             var testObject = new BindableObjectForTest();
 
             testObject.PropertyChanged += (s, e) =>
-                                              {
-                                                  // Some dummy calculation that takes up some time
-                                                  var d = 1.234d;
+                {
+                    // Some dummy calculation that takes up some time
+                    var d = 1.234d;
 
-                                                  for (var k = 0;k < 1000;++k)
-                                                  {
-                                                      d = Math.Cos(d);
-                                                      d = Math.Sin(d * d);
-                                                      d = Math.Sqrt(d);
-                                                      d = Math.Log10(d) * d;
-                                                  }
-                                              };
-            for (var i = 0;i < 6;++i)
-            {
+                    for (var k = 0;k < 1000;++k) {
+                        d = Math.Cos(d);
+                        d = Math.Sin(d * d);
+                        d = Math.Sqrt(d);
+                        d = Math.Log10(d) * d;
+                    }
+                };
+
+            for (var i = 0;i < 6;++i) {
                 var iterations = 1 + i * 500;
 
                 Console.WriteLine("{0,-5} times using {1,25} => {2} ms", iterations, "UseString", TimeLoop(iterations, () => { testObject.UseString = "XX"; }));
-                Console.WriteLine("{0,-5} times using {1,25} => {2} ms", iterations, "UseStringVerified", TimeLoop(iterations, () => { testObject.UseStringVerified = "XX"; }));
+                Console.WriteLine("{0,-5} times using {1,25} => {2} ms", iterations, "UseStringVerified",
+                                  TimeLoop(iterations, () => { testObject.UseStringVerified = "XX"; }));
                 Console.WriteLine("{0,-5} times using {1,25} => {2} ms", iterations, "UseMethodBase", TimeLoop(iterations, () => { testObject.UseMethodBase = "XX"; }));
-                Console.WriteLine("{0,-5} times using {1,25} => {2} ms", iterations, "UseMethodBaseVerified", TimeLoop(iterations, () => { testObject.UseMethodBaseVerified = "XX"; }));
+                Console.WriteLine("{0,-5} times using {1,25} => {2} ms", iterations, "UseMethodBaseVerified",
+                                  TimeLoop(iterations, () => { testObject.UseMethodBaseVerified = "XX"; }));
                 Console.WriteLine("{0,-5} times using {1,25} => {2} ms", iterations, "UseExpression", TimeLoop(iterations, () => { testObject.UseExpression = "XX"; }));
-                Console.WriteLine("{0,-5} times using {1,25} => {2} ms", iterations, "UseExpressionVerified", TimeLoop(iterations, () => { testObject.UseExpressionVerified = "XX"; }));
+                Console.WriteLine("{0,-5} times using {1,25} => {2} ms", iterations, "UseExpressionVerified",
+                                  TimeLoop(iterations, () => { testObject.UseExpressionVerified = "XX"; }));
                 Console.WriteLine();
             }
         }
@@ -50,8 +52,7 @@ namespace Wanderer.Library.Common.ConsoleTests
         {
             Contract.Requires<ArgumentNullException>(action != null);
 
-            try
-            {
+            try {
                 #region Garbage collect
                 GC.Collect();
                 Thread.Sleep(1);
@@ -78,8 +79,7 @@ namespace Wanderer.Library.Common.ConsoleTests
 
                 return watch.ElapsedMilliseconds.ToString();
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 return ex.Message;
             }
         }
