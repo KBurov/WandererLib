0f1d4e8f741f78b34bac6d1dd8190ab7f203b220
Konstantin Burov <burov.konstantin@gmail.com>
2015-01-30 16:09:52 +0200

    Add code contracts and xml comments

--- Extensions/Threading/EnumerableAsyncExtentions.cs
+++ Extensions/Threading/EnumerableAsyncExtentions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,23 +16,63 @@ namespace Wanderer.Library.Extensions.Threading
     public static class EnumerableAsyncExtentions
     {
         #region Consuming
+        /// <summary>
+        /// Consume elements from <paramref name="producer"/> in async way through internal thread-safe collection.
+        /// </summary>
+        /// <typeparam name="T">type of elements in <paramref name="producer"/></typeparam>
+        /// <param name="producer">source of elements for consuming</param>
+        /// <param name="boundedCapacity">bounded size of the internal collection</param>
+        /// <returns>enumeration of elements from <paramref name="producer"/></returns>
         public static IEnumerable<T> ConsumeWithQueue<T>(this IEnumerable<T> producer, int boundedCapacity = 0)
         {
+            Contract.Requires<ArgumentNullException>(producer != null, "producer cannot be null");
+
             return ConsumeWithQueue(producer, CancellationToken.None, null, boundedCapacity);
         }
 
+        /// <summary>
+        /// Consume elements from <paramref name="producer"/> in async way through internal thread-safe collection.
+        /// </summary>
+        /// <typeparam name="T">type of elements in <paramref name="producer"/></typeparam>
+        /// <param name="producer">source of elements for consuming</param>
+        /// <param name="cancellationToken">notify when cancellation is needed</param>
+        /// <param name="boundedCapacity">bounded size of the internal collection</param>
+        /// <returns>enumeration of elements from <paramref name="producer"/></returns>
         public static IEnumerable<T> ConsumeWithQueue<T>(this IEnumerable<T> producer, CancellationToken cancellationToken, int boundedCapacity = 0)
         {
+            Contract.Requires<ArgumentNullException>(producer != null, "producer cannot be null");
+
             return ConsumeWithQueue(producer, cancellationToken, null, boundedCapacity);
         }
 
+        /// <summary>
+        /// Consume elements from <paramref name="producer"/> in async way through internal thread-safe collection.
+        /// </summary>
+        /// <typeparam name="T">type of elements in <paramref name="producer"/></typeparam>
+        /// <param name="producer">source of elements for consuming</param>
+        /// <param name="producedCountTracker">action to count produced elements</param>
+        /// <param name="boundedCapacity">bounded size of the internal collection</param>
+        /// <returns>enumeration of elements from <paramref name="producer"/></returns>
         public static IEnumerable<T> ConsumeWithQueue<T>(this IEnumerable<T> producer, Action<int> producedCountTracker, int boundedCapacity = 0)
         {
+            Contract.Requires<ArgumentNullException>(producer != null, "producer cannot be null");
+
             return ConsumeWithQueue(producer, CancellationToken.None, producedCountTracker, boundedCapacity);
         }
 
+        /// <summary>
+        /// Consume elements from <paramref name="producer"/> in async way through internal thread-safe collection.
+        /// </summary>
+        /// <typeparam name="T">type of elements in <paramref name="producer"/></typeparam>
+        /// <param name="producer">source of elements for consuming</param>
+        /// <param name="cancellationToken">notify when cancellation is needed</param>
+        /// <param name="producedCountTracker">action to count produced elements</param>
+        /// <param name="boundedCapacity">bounded size of the internal collection</param>
+        /// <returns>enumeration of elements from <paramref name="producer"/></returns>
         public static IEnumerable<T> ConsumeWithQueue<T>(this IEnumerable<T> producer, CancellationToken cancellationToken, Action<int> producedCountTracker, int boundedCapacity = 0)
         {
+            Contract.Requires<ArgumentNullException>(producer != null, "producer cannot be null");
+
             var queue = boundedCapacity > 0
                 ? new BlockingCollection<T>(boundedCapacity)
                 : new BlockingCollection<T>();
