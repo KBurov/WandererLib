7dc13466e99d10f7dd0fc89f1217c7401be7c4a8
Konstantin Burov <konstantin.burov@hotmail.com>
2015-02-11 02:01:17 +0200

    Add synchronization Windows API support

---
+++ WindowsApi/Synchronization/NativeMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+
+using Wanderer.Library.WindowsApi.SafeHandles;
+
+namespace Wanderer.Library.WindowsApi.Synchronization
+{
+    /// <summary>
+    /// Synchronization Windows API functions.
+    /// </summary>
+    internal static class NativeMethods
+    {
+        private const string Kernel32 = "kernel32.dll";
+
+        /// <summary>
+        /// Infinite wait timeout value.
+        /// </summary>
+        public const uint InfiniteTimeout = 0xFFFFFFFF;
+
+        #region WaitForSingleObject
+        public static bool WaitForSingleObject(SafeTokenHandle tokenHandle, uint milliseconds = InfiniteTimeout)
+        {
+            Contract.Requires<ArgumentNullException>(tokenHandle != null, "tokenHandle cannot be null");
+
+            var waitResult = (WaitResult) WaitForSingleObject(tokenHandle.DangerousGetHandle(), milliseconds);
+
+            if (waitResult == WaitResult.Failed) {
+                WindowsApi.NativeMethods.ReportWin32Exception();
+            }
+
+            return waitResult != WaitResult.Timeout;
+        }
+
+        [DllImport(Kernel32, SetLastError = true)]
+        private static extern uint WaitForSingleObject(
+            [In] IntPtr hHandle,
+            [In] uint dwMilliseconds);
+        #endregion
+    }
+}
---
+++ WindowsApi/Synchronization/WaitResult.cs
@@ -0,0 +1,27 @@
+﻿namespace Wanderer.Library.WindowsApi.Synchronization
+{
+    /// <summary>
+    /// Indicates the event that describes wait result.
+    /// </summary>
+    internal enum WaitResult : uint
+    {
+        /// <summary>
+        /// The state of the specified object is signaled.
+        /// </summary>
+        Object0 = 0x00000000,
+        /// <summary>
+        /// The specified object is a mutex object that was not released by the thread that owned the mutex object before the owning thread terminated.
+        /// Ownership of the mutex object is granted to the calling thread and the mutex state is set to nonsignaled.
+        /// If the mutex was protecting persistent state information, you should check it for consistency.
+        /// </summary>
+        Abandoned = 0x00000080,
+        /// <summary>
+        /// The time-out interval elapsed, and the object's state is nonsignaled.
+        /// </summary>
+        Timeout = 0x00000102,
+        /// <summary>
+        /// The function has failed.
+        /// </summary>
+        Failed = 0xFFFFFFFF
+    }
+}
