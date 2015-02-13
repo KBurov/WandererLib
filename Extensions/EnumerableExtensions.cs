8bfbce1f48507c1682b34b280e9b50e1b43a1139
Konstantin Burov <burov.konstantin@gmail.com>
2015-02-13 17:40:59 +0200

    Add Enumerable extensions (temporary commit)

---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Wanderer.Library.Extensions
+{
+    /// <summary>
+    /// Extension and helper methods to process <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Create object with <see cref="IEnumerator{T}"/> interface to enumerate through merged sorted enumerables.
+        /// </summary>
+        /// <typeparam name="T">type of enumerable elements</typeparam>
+        /// <param name="sources">collections of sorted enumerables</param>
+        /// <param name="comparer">enumerable elemenets comparer (<see cref="IComparer{T}"/></param>
+        /// <returns>object with <see cref="IEnumerator{T}"/> interface</returns>
+        public static IEnumerator<T> GetSortedEnumerableMergeEnumerator<T>(this IEnumerable<T>[] sources, IComparer<T> comparer)
+        {
+            Contract.Requires<ArgumentNullException>(sources != null, "sources cannot be null");
+            Contract.Requires<ArgumentNullException>(comparer != null, "comparer cannot be null");
+
+            return new SortedEnumerableMergeEnumerator<T>(sources, comparer);
+        }
+
+        //public static IEnumerable<T> MergeSortedEnumerable<T>(IEnumerable<T>[] sources, IComparer<T> comparer)
+        //{
+        //    Contract.Requires<ArgumentNullException>(sources != null, "sources cannot be null");
+        //    Contract.Requires<ArgumentNullException>(comparer != null, "comparer cannot be null");
+
+        //    if (sources.Length == 0) {
+        //        return Enumerable.Empty<T>();
+        //    }
+
+        //    if (sources.Length == 1) {
+        //        return sources[0];
+        //    }
+
+        //    var enumerators = sources
+        //        .Select(list => list.GetEnumerator())
+        //        .Where(e => e.MoveNext());
+        //}
+    }
+}
---
+++ Extensions/SortedEnumerableMergeEnumerator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace Wanderer.Library.Extensions
+{
+    internal sealed class SortedEnumerableMergeEnumerator<T> : IEnumerator<T>
+    {
+        private const string ObjectDisposedExceptionMessage = "IEnumerator already disposed";
+
+        private readonly IEnumerable<T>[] _sources;
+        private readonly IComparer<T> _comparer;
+
+        /// <summary>
+        /// Determines whether object already disposed or not.
+        /// </summary>
+// ReSharper disable MemberCanBePrivate.Global
+        public bool IsDisposed { get; private set; }
+// ReSharper restore MemberCanBePrivate.Global
+
+        #region IEnumerator<T> implementation
+        /// <summary>
+        /// Gets the element in the collection at the current position of the enumerator.
+        /// </summary>
+        public T Current
+        {
+            get
+            {
+                Contract.Requires<ObjectDisposedException>(!IsDisposed, ObjectDisposedExceptionMessage);
+
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Advances the enumerator to the next element of the collection.
+        /// </summary>
+        /// <returns>true if the enumerator was successfully advanced to the next element; false if the enumerator has passed the end of the collection</returns>
+        public bool MoveNext()
+        {
+            Contract.Requires<ObjectDisposedException>(!IsDisposed, ObjectDisposedExceptionMessage);
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sets the enumerator to its initial position, which is before the first element in the collection.
+        /// </summary>
+        public void Reset()
+        {
+            Contract.Requires<ObjectDisposedException>(!IsDisposed, ObjectDisposedExceptionMessage);
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the current element in the collection.
+        /// </summary>
+        object IEnumerator.Current { get { return Current; } }
+        #endregion
+
+        #region IDisposable implementation
+        /// <summary>
+        /// <see cref="IDisposable"/> interface implementation.
+        /// </summary>
+        void IDisposable.Dispose()
+        {
+            Contract.Ensures(IsDisposed);
+
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            Contract.Ensures(IsDisposed);
+
+            if (IsDisposed) {
+                return;
+            }
+
+            if (disposing) {
+                //
+            }
+
+            IsDisposed = true;
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initialize constructor.
+        /// </summary>
+        /// <param name="sources"></param>
+        /// <param name="comparer"></param>
+        public SortedEnumerableMergeEnumerator(IEnumerable<T>[] sources, IComparer<T> comparer)
+        {
+            Contract.Requires<ArgumentNullException>(sources != null, "sources cannot be null");
+            Contract.Requires<ArgumentNullException>(comparer != null, "comparer cannot be null");
+
+            _comparer = comparer;
+            _sources = sources;
+        }
+
+        /// <summary>
+        /// Finalizer.
+        /// </summary>
+        ~SortedEnumerableMergeEnumerator()
+        {
+            Dispose(false);
+        }
+        #endregion
+    }
+}
